f15db75774ca00fd21d92be0ae0a1f51482618da
Mihir Chaturvedi <plibither8@gmail.com>
2020-04-19 16:04:59 +0530

    Fix: Add color data

--- scripts/repo-details.js
+++ scripts/repo-details.js
@@ -13,6 +13,28 @@ const fetchOptions = {
 	}
 };
 
+// this will hold all the color data
+let colorData;
+// get a language color data
+const getLanguageColor = async name => {
+	// default color
+	let color = '#cccccc';
+
+	// important checks
+	if (colorData[name]) {
+		if (colorData[name].color) {
+			color = colorData[name].color;
+		} else {
+			const {group} = colorData[name]
+			if (group && colorData[group].color) {
+				color = colorData[group].color;
+			}
+		}
+	}
+
+	return color;
+}
+
 const getRepoDetails = async repo => {
 	const response = await fetch(`https://api.github.com/repos/${repo}`, fetchOptions)
 		.then(res => res.json());
@@ -20,11 +42,16 @@ const getRepoDetails = async repo => {
 		stars: response.stargazers_count,
 		updated: new Date(response.pushed_at).getTime(),
 		created: new Date(response.created_at).getTime(),
-		language: response.language
+		language: response.language,
+		color: await getLanguageColor(response.language)
 	};
 }
 
 const main = async () => {
+	colorData = await fetch('https://rawgit.com/github/linguist/master/lib/linguist/languages.yml')
+		.then(res => res.text())
+		.then(text => yaml.safeLoad(text));
+
 	const yamlText = await readFile(yamlFile);
 	const data = yaml.safeLoad(yamlText);
 
